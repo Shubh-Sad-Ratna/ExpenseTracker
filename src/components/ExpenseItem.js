dc7789ac78b528755ea9308559b02c9351146613
Shubh-Sad-Ratna <shubhamband2000@gmail.com>
2024-08-23 10:06:52 +0530

    components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <p>Food 70rs</p>
+            <p>Petrol 66rs</p>
+            <p>Movies 99rs</p>
+        </div>
+    )
+}
+export default ExpenseItem;
