2ab900a1e04aeb425e51e2b84c7f883f586fa8f8
Shubh-Sad-Ratna <shubhamband2000@gmail.com>
2024-08-28 09:10:35 +0530

    Challenge - Render Expense Form Conditionally

--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 import "./NewExpenses.css"
 import ExpenseForm from "./ExpenseForm";
 const NewExpenses=(props)=>{
+    const [edit,setEdit]=useState(false);
     const submitDataHandler=(data)=>{
         data={...data,id:Math.random().toString()}
         props.onSubmit(data);
     }
     return(
         <div className="new-expense"> 
-            <ExpenseForm onSubmit={submitDataHandler}/>
+            {edit===false && <button onClick={()=>{
+                setEdit(true)
+            }} type="button">Add Expense</button>}
+
+            {edit===true && <ExpenseForm onSubmit={submitDataHandler} onCancel={()=>{
+                setEdit(false);
+            }}/>}
+            
         </div>
     )
 }
